21b5768ff541fcfc1039317531c8d47105eeaede
Amber L Lowe <52145666+amberlowe1001@users.noreply.github.com>
2020-01-16 16:52:05 -0800

    inital commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,30 +1,30 @@
-/* Step 1: using axios, send a GET request to the following URL 
+/* Step 1: using axios, send a GET request to the following URL
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+/* Step 2: Inspect and study the data coming back, this is YOUR
+   github info! You will need to understand the structure of this
+   data in order to use it to build your component function
 
    Skip to Step 3.
 */
 
-/* Step 4: Pass the data received from Github into your function, 
+/* Step 4: Pass the data received from Github into your function,
            create a new component and add it to the DOM as a child of .cards
 */
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
+/* Step 5: Now that you have your own card getting added to the DOM, either
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers
+          , manually find some other users' github handles, or use the list found
           at the bottom of the page. Get at least 5 different Github usernames and add them as
           Individual strings to the friendsArray below.
-          
+
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['jvillalp','mrzacsmith','caw44200'];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -35,7 +35,7 @@ const followersArray = [];
     <h3 class="name">{users name}</h3>
     <p class="username">{users user name}</p>
     <p>Location: {users location}</p>
-    <p>Profile:  
+    <p>Profile:
       <a href={address to users github page}>{address to users github page}</a>
     </p>
     <p>Followers: {users followers count}</p>
@@ -46,10 +46,64 @@ const followersArray = [];
 
 */
 
-/* List of LS Instructors Github username's: 
+/* List of LS Instructors Github username's:
   tetondan
   dustinmyers
   justsml
   luishrd
   bigknell
 */
+/* Step 1: using axios, send a GET request to the following URL
+           (replacing the palceholder with your Github name):
+           https://api.github.com/users/<your name>
+*/
+  function createCard(user) {
+    const newCard = document.createElement('div'),
+          newImage = document.createElement('img'),
+
+         newUserName = document.createElement('h3'),
+          newCardInfo = document.createElement('div'),
+          newLocation = document.createElement('p'),
+          newProfile = document.createElement('p'),
+          newAddress = document.createElement('a'),
+          newUserFollowers = document.createElement('p'),
+          newUserFollowing = document.createElement('p'),
+          newBioUsers = document.createElement('p')
+
+          newImage.append(card);
+          newCard.append(card-info);
+          newLocation.append(card-info);
+          newProfile. append(card-info);
+          newAddress.append(card-info);
+          newUserFollowers.append(card-info);
+          newUserFollowing.append(card-info);
+          newBiosUsers.append(card-info);
+
+
+
+
+newUserName.textContent ='name: Username';
+newLocation.textContent ='user: location';
+newProfile.textContent = 'user: profile';
+newAddress.textContent = 'user: address';
+newUsersFollowers = 'user: following';
+newUsersFollowers = 'users:followers';
+newBioUsers = 'users:bio';
+
+newImage.src = imgUrl;
+newImage.classList.add('card');
+newCard.classlist.add('card');
+newCardInfo.classlist.add('card-info')
+newUserName.classlist.add('card-info');
+newProfile.classList.add('card-info')
+return newCard;
+
+  }
+
+
+axios.get('https://api.github.com/users/ amberlowe1001');
+.then(followersArray => {
+  console.log(response);
+  response.data.message.forEach(item =>{
+    const
+  })
